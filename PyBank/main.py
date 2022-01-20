10213da9c644e33b22eaf80ca1168e1247e1b7d7
Nii <inodarteifio@gmail.com>
2022-01-20 11:40:30 -0500

    upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,17 @@
+#import the needed packages
 import os
 import csv
 import numpy as np
 
+#creating the path to the csv file
 pybank_csv= os.path.join("Resources","budget_data.csv")
 
+#create three empty lists to store the contents of the  each column in the csv
 date= []
 pl= []
 pl_int=[]
 
+#open the csv file and storing the columns in the empty lists date and pl
 with open(pybank_csv, newline='', encoding= 'utf-8') as csvfile:
     csvreader= csv.reader(csvfile, delimiter= ',')
     for row in csvreader:
@@ -16,40 +20,42 @@ with open(pybank_csv, newline='', encoding= 'utf-8') as csvfile:
 
         pl.append(row[1])
 
+    #removing the first entry from each list since that entry is just the column names (Date & Profit/Loss).
     date.pop(0)
     pl.pop(0)
 
+    #converting the strings in the pl list to int and storing them in a different list pl_int
     for i in range(0, len(pl)):
         pl_int.append(int(pl[i]))
 
-
+    #calculating the total nuber of months
     total_month= len(date)
     total=0
+
+    #calculating the net total amount of "Profit/Loss" over the entire period
     for num in pl_int:
         total+= num
     
-
+    #calculating the changes in "Profit/Loss" over the entire period. 
+    #Used numpy to find the difference between consecutive numbers which is stored as a list in 'difference'
+    #The changes in "Profit/Loss" is the sum of 'difference list' divided by the length of the list
     difference= np.diff(pl_int)
     avgchange= difference.sum()/len(difference)
     
+    #Found the max profit from the 'pl_int' list then used '.index' to find the position of this value.
+    #The position of the max profit is stored in 'max_date_index' which is later used to pull the corresponding date to this max value from the 'date' list.
     maximum= max(pl_int)
     max_date_index= pl_int.index(maximum)
     max_date= date[max_date_index]
 
+    #Found the min profit from the 'pl_int' list then used '.index' to find the position of this value.
+    #The position of the min profit is stored in 'min_date_index' which is later used to pull the corresponding date to this min value from the 'date' list.
     minimum= min(pl_int)
     min_date_index= pl_int.index(minimum)
     min_date= date[min_date_index]
 
-    report1= (
-    print("__________Financial Analysis__________"),
-    print("Total Months:                    " +str(total_month)),
-    print('Total:                           '+ '$'+str(total)),
-    print('Average Change:                  '+'$'+str(avgchange)),
-    print('Greatest Increase in Profits:    '+ max_date+ '  $'+str(maximum)),
-    print('Greatest Decrease in Profits:    '+ min_date+ '  $'+str(minimum)))
     
-    print(report1)
-
+    #Storing all print statements to the variable output and later pringting that to the terminal
     output = (
     f"Financial Analysis\n"
     f"------------------------------------------------------------\n"
@@ -62,17 +68,14 @@ with open(pybank_csv, newline='', encoding= 'utf-8') as csvfile:
     print(output)
     
 
+#creating a new text file to store the analysis in the 'Analysis' folder
 
-with open('PyBank.txt','w') as final:
-    
+output_path = os.path.join('..','Analysis','PyBank.txt')
+
+
+with open(output_path,'w') as final:
     
     final.write(output)
-
-
-
-
-
-
 
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,14 +1,18 @@
+#import the needed packages
 import os
 import csv
 import numpy as np
 
+#creating the path to the csv file
 election_csv= os.path.join("Resources","election_data.csv")
 
+#create three empty lists to store the contents of the  each column in the csv and one extra list to store 'integers'
 voter_ID= []
 county= []
 candidates=[]
 candidate= []
 
+#open the csv file and storing the columns in the empty lists 'voter_ID', 'county' and 'candidates'
 with open(election_csv, newline='', encoding= 'utf-8') as csvfile:
     csvreader= csv.reader(csvfile, delimiter= ',')
     for row in csvreader:
@@ -17,13 +21,21 @@ with open(election_csv, newline='', encoding= 'utf-8') as csvfile:
         county.append(row[1])
         candidates.append(row[2])
 
+    #removing the first entry from each list since that entry is just the column names
+    voter_ID.pop(0)
+    county.pop(0)
+    candidates.pop(0)
+
+
+    #find the length of any column to see the number of people that voted
     total_votes= len(voter_ID)
 
+    # I created a seprate list to store the names of the candidates who ran for the position
     for name in candidates:
         if name not in candidate:
             candidate.append(name)
 
-
+# I used for loops to count the number of times a candidates name appered in the 'candidates' list to get the total votes for that candidate
     Khan_count=0
     for i in candidates:
         if i== 'Khan':
@@ -46,17 +58,27 @@ with open(election_csv, newline='', encoding= 'utf-8') as csvfile:
 
 
     
-
+   # Calculating the percentage of votes each candidate had and formatting that number to a percentage with three decimal places.
     per_khan= "{:.3%}".format(Khan_count/total_votes)
     per_correy= "{:.3%}".format(Correy_count/total_votes)
     per_li= "{:.3%}".format(Li_count/total_votes)
     per_otooley= "{:.3%}".format(Otooley_count/total_votes)
 
+   #finding the winner from a list of total votes usinf if statements to find the max ot the 'winner_list'.
+    winner_list= [Khan_count, Correy_count, Li_count, Otooley_count]
+    winner=""
+    for val in winner_list:
+        if max(winner_list) is Khan_count:
+            winner= 'Khan'
+        elif max(winner_list) is Correy_count:
+            winner= 'Correy'
+        elif max(winner_list) is Li_count:
+            winner= 'Li'
+        elif max(winner_list) is Otooley_count:
+            winner= "O'Tooley"
 
-    #print(per_otooley)      
-    #print(per_li)   
 
-
+    #Storing all print statements to the variable output and later pringting that to the terminal
     output = (
     f"Election Results\n"
     f"----------------------------\n"
@@ -67,16 +89,17 @@ with open(election_csv, newline='', encoding= 'utf-8') as csvfile:
     f"Li:         {per_li} ({Li_count})\n"
     f"O'Tooley:   {per_otooley} ({Otooley_count})\n"
     f"----------------------------\n"
-    #f"Winner: {greatest_decrease_date} \n"
+    f"Winner:     {winner} \n"
+    f"----------------------------\n"
     )
 
 # Print the output (to terminal)
     print(output)
 
 
+#creating a new text file to store the analysis in the 'Analysis' folder
+output_path = os.path.join('..','Analysis','PyPoll.txt')
 
-with open('PyPoll.txt','w') as final:
-    
+with open(output_path,'w') as final:
     
     final.write(output)
-
