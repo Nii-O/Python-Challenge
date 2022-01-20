2816cb20538418e1fb23a3d09bee1d45ad678505
Nii <inodarteifio@gmail.com>
2022-01-19 22:45:55 -0500

    upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,99 @@
+import os
+import csv
+import numpy as np
+
+pybank_csv= os.path.join("Resources","budget_data.csv")
+
+date= []
+pl= []
+pl_int=[]
+
+with open(pybank_csv, newline='', encoding= 'utf-8') as csvfile:
+    csvreader= csv.reader(csvfile, delimiter= ',')
+    for row in csvreader:
+
+        date.append(row[0])
+
+        pl.append(row[1])
+
+    date.pop(0)
+    pl.pop(0)
+
+    for i in range(0, len(pl)):
+        pl_int.append(int(pl[i]))
+
+
+    total_month= len(date)
+    total=0
+    for num in pl_int:
+        total+= num
+    
+
+    difference= np.diff(pl_int)
+    avgchange= difference.sum()/len(difference)
+    
+    maximum= max(pl_int)
+    max_date_index= pl_int.index(maximum)
+    max_date= date[max_date_index]
+
+    minimum= min(pl_int)
+    min_date_index= pl_int.index(minimum)
+    min_date= date[min_date_index]
+
+    report1= (
+    print("__________Financial Analysis__________"),
+    print("Total Months:                    " +str(total_month)),
+    print('Total:                           '+ '$'+str(total)),
+    print('Average Change:                  '+'$'+str(avgchange)),
+    print('Greatest Increase in Profits:    '+ max_date+ '  $'+str(maximum)),
+    print('Greatest Decrease in Profits:    '+ min_date+ '  $'+str(minimum)))
+    
+    print(report1)
+
+    output = (
+    f"Financial Analysis\n"
+    f"------------------------------------------------------------\n"
+    f"Total Months:                     {total_month}\n"
+    f"Total:                            ${total}\n"
+    f"Average  Change:                  ${avgchange:.2f}\n"
+    f"Greatest Increase in Profits:     {max_date} (${maximum})\n"
+    f"Greatest Decrease in Profits:     {min_date} (${minimum})\n")
+
+    print(output)
+    
+
+
+with open('PyBank.txt','w') as final:
+    
+    
+    final.write(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#test_zip= zip(date, pl)
+
+#output_file = os.path.join("test_final")
+
+#with open(output_file, "w", newline="") as datafile:
+ #   writer = csv.writer(datafile)
+
+  #  writer.writerow(["Date","Profit/Loss"])
+
+   # writer.writerows(test_zip)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,82 @@
+import os
+import csv
+import numpy as np
+
+election_csv= os.path.join("Resources","election_data.csv")
+
+voter_ID= []
+county= []
+candidates=[]
+candidate= []
+
+with open(election_csv, newline='', encoding= 'utf-8') as csvfile:
+    csvreader= csv.reader(csvfile, delimiter= ',')
+    for row in csvreader:
+
+        voter_ID.append(row[0])
+        county.append(row[1])
+        candidates.append(row[2])
+
+    total_votes= len(voter_ID)
+
+    for name in candidates:
+        if name not in candidate:
+            candidate.append(name)
+
+
+    Khan_count=0
+    for i in candidates:
+        if i== 'Khan':
+            Khan_count+= 1
+
+    Correy_count=0
+    for j in candidates:
+        if j== 'Correy':
+            Correy_count+= 1
+
+    Li_count=0
+    for k in candidates:
+        if k== 'Li':
+            Li_count+= 1
+
+    Otooley_count=0
+    for l in candidates:
+        if l== "O'Tooley":
+            Otooley_count+= 1
+
+
+    
+
+    per_khan= "{:.3%}".format(Khan_count/total_votes)
+    per_correy= "{:.3%}".format(Correy_count/total_votes)
+    per_li= "{:.3%}".format(Li_count/total_votes)
+    per_otooley= "{:.3%}".format(Otooley_count/total_votes)
+
+
+    #print(per_otooley)      
+    #print(per_li)   
+
+
+    output = (
+    f"Election Results\n"
+    f"----------------------------\n"
+    f"Total Votes: {total_votes}\n"
+    f"----------------------------\n"
+    f"Khan:       {per_khan} ({Khan_count})\n"
+    f"Correy:     {per_correy} ({Correy_count})\n"
+    f"Li:         {per_li} ({Li_count})\n"
+    f"O'Tooley:   {per_otooley} ({Otooley_count})\n"
+    f"----------------------------\n"
+    #f"Winner: {greatest_decrease_date} \n"
+    )
+
+# Print the output (to terminal)
+    print(output)
+
+
+
+with open('PyPoll.txt','w') as final:
+    
+    
+    final.write(output)
 
